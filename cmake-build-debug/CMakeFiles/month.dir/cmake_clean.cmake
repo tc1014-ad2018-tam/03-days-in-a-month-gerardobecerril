f0b2e698f9913aa4013077e7129e2d242d877d39
Gerardo Becerril <gerbec2000@gmail.com>
2018-08-28 19:30:29 -0500

    third commit

---
+++ cmake-build-debug/CMakeFiles/month.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/month.dir/main.c.o"
+  "month.pdb"
+  "month"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/month.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
